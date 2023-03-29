bc8947170a5ede6c0d564ee7dc7998026bcf920f
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-03-29 15:37:47 -0400

    adds create table for required schema structure

--- db/migrate/20230329193049_create_tasks.rb
+++ db/migrate/20230329193049_create_tasks.rb
@@ -1,4 +1,11 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
+    create_table :tasks do |t|
+      t.string :name
+      t.integer :important
+      t.integer :urgent
+      t.integer :complete
+      t.integer :task_list_id
+    end
   end
 end
