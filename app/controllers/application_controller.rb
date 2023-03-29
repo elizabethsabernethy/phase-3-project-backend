a74da42fc893fceaefe9891ef2de06619de0fc1a
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-03-29 16:00:03 -0400

    adds post request for task-lists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,41 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/task-lists" do
-    taskLists = TaskList.all 
-    taskLists.to_json(include: :tasks)
+    task_lists = TaskList.all 
+    task_lists.to_json(include: :tasks)
   end
+
+  post "/task-lists" do
+    task_list = TaskList.create(
+      name: params[:name],
+      important: params[:important],
+      urgent: params[:urgent],
+      complete: params[:complete]
+    )
+    task_list.to_json
+  end
+
+  # post '/baked_goods' do
+  #   baked_good = BakedGood.create(
+  #     name: params[:name],
+  #     price: params[:price],
+  #     bakery_id: params[:bakery_id]
+  #   )
+  #   baked_good.to_json
+  # end
+
+  # patch '/bakeries/:id' do
+  #   bakery = Bakery.find(params[:id])
+  #   bakery.update(
+  #     name: params[:name]
+  #   )
+  #   bakery.to_json
+  # end
+
+  # delete '/baked_goods/:id' do
+  #   baked_good = BakedGood.find(params[:id])
+  #   baked_good.destroy
+  #   baked_good.to_json
+  # end
 
 end
