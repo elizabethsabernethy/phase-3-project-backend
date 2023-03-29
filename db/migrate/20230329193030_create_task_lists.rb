d5d315ee4fe974a4c54010c629bbe92f6207b999
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-03-29 15:35:05 -0400

    adds create table code to change method for required schema structure

--- db/migrate/20230329193030_create_task_lists.rb
+++ db/migrate/20230329193030_create_task_lists.rb
@@ -1,4 +1,10 @@
 class CreateTaskLists < ActiveRecord::Migration[6.1]
   def change
+    create_table :task_lists do |t|
+      t.string :name
+      t.integer :important
+      t.integer :urgent
+      t.integer :complete
+    end
   end
 end
